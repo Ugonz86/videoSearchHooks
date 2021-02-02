d8e8b5bd3ba8239512045324b4e3cf79b369c640
Ugonz86 <ugonzalez86@gmail.com>
2021-02-02 11:38:56 -0800

    Fix warnings and defaulting video selection

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,14 @@ import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 import './App.css';
 
-const KEY ='AIzaSyASdAXfv2bMZfjxsjh-GDRfAZ9Ev7Yjc_Q';
+const KEY = 'AIzaSyASdAXfv2bMZfjxsjh-GDRfAZ9Ev7Yjc_Q';
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
 
   //asynchronous api request
   onTermSubmit = async term => {
@@ -21,7 +25,11 @@ class App extends React.Component {
         key: KEY
       }
     });
-    this.setState({ videos: response.data.items })
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = (video) => {
@@ -32,10 +40,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList 
-        onVideoSelect={this.onVideoSelect} 
-        videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -6,13 +6,16 @@ import './App.css';
 const VideoItem = ({ video, onVideoSelect }) => {
     return (
         <div onClick={() => onVideoSelect(video)} className="video-item">
-            <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+            <img
+                alt={video.snippet.title}
+                className="ui image"
+                src={video.snippet.thumbnails.medium.url}
+            />
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
             </div>
         </div>
     );
-
 };
 
 export default VideoItem;
