93c32f4d1e7bc5d1010810e59714d25809f2b6c7
Ugonz86 <ugonzalez86@gmail.com>
2021-02-01 22:41:20 -0800

    Initial commit, project set up

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
